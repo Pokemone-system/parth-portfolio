410293c955b69a8661a58bbe6e79ce53d97e31af
p-chotaliya <chotaliyaparth.pc@gmail.com>
2023-06-08 19:48:45 -0400

    first commit

---
+++ src/ExData.js
@@ -0,0 +1,61 @@
+const workHistory = [
+  {
+    companyName: "Digimaze",
+    role: "Fullstack Software Developer",
+    duration: "Aug'22 – March'23",
+    workDes: [
+      "Developed the Review and Referral Service System by utilizing NextJS (React Framework) and MongoDB database, and implemented state management with React Redux library",
+      "Leveraged Amazon Lambda services to create serverless node APIs for the Review and Referral Service System.",
+      "Collaborated with a UI/UX designer to transform the design into React Components for the Review and Referral Service System.",
+      "Constructed a web-based File Sharing and Profit Analysis application with Angular 5 and NodeJS.",
+      "Created a Relational Database using MySQL Workbench to support the File Sharing and Profit Analysis application.",
+      "Configured and deployed the web application on a Digital Ocean and AWS server for seamless accessibility",
+    ],
+    exposer: [
+      "React",
+      "Next",
+      "Redux",
+      "NodeJs",
+      "ExpressJs",
+      "MongoDB",
+      "Amazon Lamda",
+      "Amazon AWS",
+    ],
+    link: "https://digimaze.io/",
+  },
+  {
+    companyName: "Infosys Limited",
+    role: "Senior System Engineer",
+    duration: "Oct'20 – Jul'22",
+    workDes: [
+      "Developed the ETL process in Informatica Data Quality to facilitate the migration of legacy data into Informatica MDM.",
+      "Customized Rest APIs for Informatica business entity service to enable efficient retrieval of large amounts of data.",
+      "Maintained the codebase for a Java EE web application using JSP, servlet, JDBC, and Oracle Essbase",
+      "Enhanced Rest and SOAP APIs with Java, Oracle Essbase, Oraacle SQL and PL/SQL database to enable custom report generation, and utilized Extended JavaScript for the development of custom reports UI",
+      "Collaborated on data migration projects with major clients CN Rails and BNSF Railways",
+    ],
+    exposer: [
+      "Informatica MDM ",
+      "JAVA",
+      "Spring Boot",
+      "Apahe Tomcat",
+      "Oracle Essbase",
+      "Amazon AWS",
+    ],
+    link: "https://www.infosys.com/",
+  },
+  {
+    companyName: "Infosys Limited",
+    role: "System Engineer Intern",
+    duration: "Jan'20 – Mar'20",
+    workDes: [
+      "Completed comprehensive training in Python, Java, Data Structures, RDBMS, and PL/SQL.",
+      "Developed a Python automation script for web scraping as part of a project.",
+      "Trained in NodeJS and Java to design and develop microservices and Rest APIs for scalable web applications.",
+    ],
+    exposer: ["Python", "JAVA", "PL/SQL", "Oracle", "Data Structure"],
+    link: "https://www.infosys.com/",
+  },
+];
+
+export default workHistory;
---
+++ src/projectData.js
@@ -0,0 +1,36 @@
+const projects = [
+  {
+    title: "FileShare and Profit Analysis System",
+    image: "./images/Fileshare.png",
+    description:
+      "This web app, built with Angular, Node.js, and MySQL, simplifies profit analysis by extracting data from uploaded Excel sheets. Analyze product profitability, view interactive dashboards, generate customizable reports, and ensure data security. Gain actionable insights to drive business success.",
+    tools: [
+      "Angular",
+      "Node Js",
+      "MySQL",
+      "HTML",
+      "CSS",
+      "Java",
+      "Private Repository",
+    ],
+    github: "https://github.com/p-chotaliya",
+  },
+  {
+    title: "AR Application",
+    image: "./images/AR.jpg",
+    description:
+      "THis Augmented Reality Facial Landmark Application, developed using Python and OpenGL, leverages facial landmark detection to superimpose virtual glasses models onto human faces. By utilizing cutting-edge technologies, we have created a seamless and interactive experience that allows users to visualize how different spectacles look on their own faces. The project was successfully presented with a comprehensive demo to a distinguished panel of professors and department members, showcasing its technical prowess and potential applications.",
+    tools: ["Python", "OpenGL"],
+    github: "https://github.com/p-chotaliya/AR-App-Python",
+  },
+  {
+    title: "Smart Assessment System",
+    image: "./images/SmartAssess.jpg",
+    description:
+      "This web-based application revolutionizes the student assessment process, incorporating an intuitive Admin dashboard for monitoring and assessing student submissions. Powered by Node.js, Express, MySQL, and Angular, this robust system features seamless integration of backend APIs and a user-friendly UI. Experience efficient assessment management and enhanced student evaluation in this cutting-edge application.",
+    tools: ["Angular", "Node Js", "HTML", "Mongodb", "CSS"],
+    github: "https://github.com/p-chotaliya/smartassessmenUI/tree/master",
+  },
+];
+
+export default projects;
